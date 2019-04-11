85d13f205293126cc5d0c3a513b96d108944233f
barth <juniior.barth@gmail.com>
2019-04-10 21:47:34 -0300

    Extracts SECRET_KEY from settings

--- pypro/settings.py
+++ pypro/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'qy+cc1xjy)fi84r^mbf+rzizjko=5j$jx&hct8&v-^hkzy@d*@'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
