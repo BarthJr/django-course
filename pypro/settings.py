52dd30cc9aa550e98414ff645b65934d5f15ddaf
Barth <joao.barth@tuntscorp.com>
2019-04-16 21:46:43 -0300

    Adds collectfast

--- pypro/settings.py
+++ pypro/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'collectfast'
+    'collectfast',
     'django.contrib.staticfiles',
     'pypro.core',
 ]
