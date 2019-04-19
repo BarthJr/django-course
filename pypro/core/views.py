04fbce1149c4e2682dae1f7f8e2394ee506a438d
barth <juniior.barth@gmail.com>
2019-04-19 13:11:12 -0300

    Adds Django Debug Toolbar

--- pypro/core/views.py
+++ pypro/core/views.py
@@ -2,4 +2,4 @@ from django.http import HttpResponse
 
 
 def home(request):
-    return HttpResponse('Hello World')
+    return HttpResponse('<html><body>Hello Django</body></html>', content_type='text/html')
